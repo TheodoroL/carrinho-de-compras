168589003b3ab8eb53d982b03dffb754c0c26b25
Lucas <lucastheodoromourofernandes@gmail.com>
2025-05-20 16:55:32 -0400

    Adicionando o arquivo js e criando uma arraylist dos produtos anteriores

---
+++ js/index.js
@@ -0,0 +1,27 @@
+//lista de produtos
+const products = [
+    {
+        id: 1,
+        name: "Teclado Mecânico",
+        category: "Periféricos",
+        price: 650.00,
+        image: "./images/teclado-mecanico.jpg",
+        quantity: 2
+    },
+    {
+        id: 2,
+        name: "Cacdeira Gamer",
+        category: "Acessório",
+        price: 1000.00,
+        image: "./images/cadeira-gamer.jpg",
+        quantity: 1
+    },
+    {
+        id: 3,
+        name: "Headset",
+        category: "Periféricos",
+        price: 500.00,
+        image: "./images/headset.jpg",
+        quantity: 1
+    }
+]
